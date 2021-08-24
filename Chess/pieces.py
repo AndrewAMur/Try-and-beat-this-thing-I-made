bf42b47724615f95c6d3223788a80beac66f1bb8
Your Name <andrewamuratov@gmail.com>
2021-08-24 10:53:09 -0400

    Gave name to object

--- Chess/pieces.py
+++ Chess/pieces.py
@@ -1,19 +1,19 @@
 class Piece:
     class Pawn:
-        def __init__(self):
-            pass
+        def __init__(self, name):
+            self.name = name
     class Bishop:
-        def __init__(self):
-            pass
+        def __init__(self, name):
+            self.name = name
     class Knight:
-        def __init__(self):
-            pass
+        def __init__(self, name):
+            self.name = name
     class Rook:
-        def __init__(self):
-            pass
+        def __init__(self, name):
+            self.name = name
     class Queen:
-        def __init__(self):
-            pass
+        def __init__(self, name):
+            self.name = name
     class King:
-        def __init__(self):
-            pass
+        def __init__(self, name):
+            self.name = name
