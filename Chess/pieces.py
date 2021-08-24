ad68da35e39109b3ca6e1a72afd745a1ee6ce3f9
Your Name <andrewamuratov@gmail.com>
2021-08-24 10:23:35 -0400

    Moved file to folder

---
+++ Chess/pieces.py
@@ -0,0 +1,19 @@
+class Piece:
+    class Pawn:
+        def __init__(self):
+            pass
+    class Bishop:
+        def __init__(self):
+            pass
+    class Knight:
+        def __init__(self):
+            pass
+    class Rook:
+        def __init__(self):
+            pass
+    class Queen:
+        def __init__(self):
+            pass
+    class King:
+        def __init__(self):
+            pass
